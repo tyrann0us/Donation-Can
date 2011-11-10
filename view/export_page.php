5d20575439ba9d4b0b1dabef53710f5ba05704dd
Jarkko Laine <jarkko@jarkkolaine.com>
2011-11-10 11:57:43 +0000

    Finished export functionality.

--- view/export_page.php
+++ view/export_page.php
@@ -17,19 +17,40 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 ?>
+<div class="wrap">
 
-<h1>Export</h1>
+<div id="icon-tools" class="icon32"><br/></div>
+<h2><?php _e("Export Data", "donation_can"); ?></h2>
 
-<p><?php _e("Include in dump:", "donation_can");?></p>
+<p><?php _e("On this page, you can download all Donation Can data from your blog to your own computer.", "donation_can");?></p>
 
-<ul>
-    <li><?php _e("Causes:", "donation_can");?> 
-        [<a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=causes&format=csv"><?php _e("CSV", "donation_can");?></a>
-        | <a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=causes&format=xml"><?php _e("XML", "donation_can");?></a>]
-    </li>
-    <li><?php _e("Donations:", "donation_can");?> [<a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=donations&format=csv"><?php _e("CSV", "donation_can");?></a>
-        | <a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=donations&format=xml"><?php _e("XML", "donation_can");?></a>]</li>
-    <li><?php _e("Settings:", "donation_can");?> [<a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=settings&format=xml"><?php _e("XML", "donation_can");?></a>]</li>
-    <li><?php _e("Styles:", "donation_can");?> [<a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=styles&format=xml"><?php _e("XML", "donation_can");?></a>]</li>
-</ul>
+<p><?php _e("Right-click on the file format you like, and use \"Save as...\" to choose the location for your file. Settings and styles are only available as XML.", "donation_can");?></p>
 
+<table border="0" id="donation-can-export-download">
+    <tr>
+        <th><?php _e("Causes:", "donation_can");?></th>
+        <td>
+            <a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=causes&format=csv"><?php _e("CSV", "donation_can");?></a>
+          | <a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=causes&format=xml"><?php _e("XML", "donation_can");?></a>
+        </td>
+    </tr>
+    <tr>
+        <th><?php _e("Donations:", "donation_can");?></th>
+        <td>
+            <a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=donations&format=csv"><?php _e("CSV", "donation_can");?></a>
+          | <a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=donations&format=xml"><?php _e("XML", "donation_can");?></a>
+        </td>
+    </tr>
+    <tr>
+        <th><?php _e("Settings:", "donation_can");?></th>
+        <td>
+            <a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=settings&format=xml"><?php _e("XML", "donation_can");?></a>
+        </td>
+    </tr>
+    <tr>
+        <th><?php _e("Styles:", "donation_can");?></th>
+        <td><a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php'), "donation_can_ajax-export");?>&action=donation_can-export&type=styles&format=xml"><?php _e("XML", "donation_can");?></a></td>
+    </tr>
+</table>
+
+</div>
