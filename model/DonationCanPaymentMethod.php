a8c79fae33c84d3ace38a1a328bf5106f5b438f5
Jarkko Laine <jarkko@jarkkolaine.com>
2011-11-30 21:50:41 +0000

    Added IPN handling to new payment method class

--- model/DonationCanPaymentMethod.php
+++ model/DonationCanPaymentMethod.php
@@ -17,6 +17,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+require_once("DonationCanDonation.php");
+
 /**
  * The parent class for all payment method implementations supported
  * by Donation Can.
